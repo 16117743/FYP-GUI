f151bf879bd3b5348d155b09634807e91eb76741
g00291875 <g00291875@gmit.ie>
2015-11-07 13:52:42 +0000

    added first line to Netbeans GUI

--- Music-Player-GUI/src/music/player/gui/MusicPlayerGUI.java
+++ Music-Player-GUI/src/music/player/gui/MusicPlayerGUI.java
@@ -15,7 +15,7 @@ public class MusicPlayerGUI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello GitHub");
     }
     
 }
